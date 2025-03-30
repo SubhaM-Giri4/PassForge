a83594a0cadd652906ea0ac7d41126453afb8e0a
Subham Kumar Giri <Subhamgiri482@gmail.com>
2025-03-31 03:09:05 +0530

    App.jsx updated

--- PassForge/src/App.jsx
+++ PassForge/src/App.jsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState ,useCallback } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length , setLength] = useState(8);
+  const [numberallowed , setNumberallowed] = useState(false);
+  const [charallowed, setCharallowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+    const passwordGenerator = useCallback(() => {
+        let pass = ""
+        let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        if (numberallowed) str += "0123456789"
+        if (charallowed) str += "!@#$%^&*-_+=[]{}~`"
+
+        for (let i = 1; i <= length; i++) {
+            let char = Math.floor(Math.random() * str.length + 1)
+            pass += str.charAt(char)
+
+        }
+
+        setPassword(pass)
+
+
+    }, [length, numberallowed, charallowed, setPassword])
 
   return (
     <>
